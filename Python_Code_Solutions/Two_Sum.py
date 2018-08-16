12ae761ab0ef2c8d14964f1135047c87e57ac092
Anand Kothari <kotharan@oregonstate.edu>
2018-08-15 22:53:17 -0700

    Made changes to comments

--- Python_Code_Solutions/Two_Sum.py
+++ Python_Code_Solutions/Two_Sum.py
@@ -26,55 +26,6 @@ class Solution(object):
 
 
 
-
-# def twoSum(self, nums, target):
-#     # hash 1
-#     hash_nums = {}
-#     for index, num in enumerate(nums):
-#         try:
-#             hash_nums[num].append(index)
-#         except KeyError:
-#             hash_nums[num] = [index]
-#     for index, num in enumerate(nums):
-#         another = target - num
-#         try:
-#             candicate = hash_nums[another]
-#             if another == num:
-#                 if len(candicate) > 1:
-#                     return candicate
-#                 else:
-#                     continue
-#             else:
-#                 return [index, candicate[0]]
-#         except KeyError:
-#             pass
-    #
-    # def twoSum(self, nums, target):
-    #     # hash 2
-    #     hash_nums = {}
-    #     for index, num in enumerate(nums):
-    #         another = target - num
-    #         try:
-    #             hash_nums[another]
-    #             return [hash_nums[another], index]
-    #         except KeyError:
-    #             hash_nums[num] = index
-
-        # def twoSum(self, nums, target):
-        #     # two point
-        #     nums_index = [(v, index) for index, v in enumerate(nums)]
-        #     nums_index.sort()
-        #     begin, end = 0, len(nums) - 1
-        #     while begin < end:
-        #         curr = nums_index[begin][0] + nums_index[end][0]
-        #         if curr == target:
-        #             return [nums_index[begin][1], nums_index[end][1]]
-        #         elif curr < target:
-        #             begin += 1
-        #         else:
-        #             end -= 1
-
-#
 if __name__ == '__main__':
     # begin
     s = Solution()
