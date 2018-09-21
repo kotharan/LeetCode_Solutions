056b5d52b14f2314e608be9f879396865882e946
Anand Kothari <kotharan@oregonstate.edu>
2018-09-21 01:23:07 -0700

    Working with vector

--- C++_Code_Solutions/Two_Sum/main.cpp
+++ C++_Code_Solutions/Two_Sum/main.cpp
@@ -3,6 +3,9 @@
 
 using namespace std;
 
+
+/* CODE TO GET VALUES WITHOUT THE USE OF VECTOR
+
 class Solution {
 public:
     int twoSum(int nums[], int target) {
@@ -20,12 +23,50 @@ public:
 
             }
 
-            }
-            }
-
-
+        }
+     }
 
     }
+};
+
+ int main()
+ {
+     Solution s;
+     int arr[] = {2,7,11,15};
+     s.twoSum(arr,9);
+ }
+
+*/
+
+
+
+
+
+
+class Solution {
+public:
+    vector<int> twoSum(int nums[], int target) {
+    vector<int> results[10];
+    for(int i=0;i<sizeof(nums);i++)
+    {
+        int number = nums[i];
+        int new_targ = target - number;
+
+        for(int j=0 ; j<sizeof(nums);j++)
+        {
+            if(nums[j]==new_targ && j!=i && j < sizeof(nums) && i < j)
+            {
+
+                results[0].push_back(i);
+                results[1].push_back(j);
+
+                return results[0,1];
+            }
+
+        }
+    }
+
+}
 };
 
 
@@ -33,5 +74,10 @@ public:
  {
      Solution s;
      int arr[] = {2,7,11,15};
-     s.twoSum(arr,9);
+     vector<int> result = s.twoSum(arr,9);
+    for(int value = 0 ; value <= result.size(); value++)
+    {
+        cout<< result[value];
+    }
+
  }
