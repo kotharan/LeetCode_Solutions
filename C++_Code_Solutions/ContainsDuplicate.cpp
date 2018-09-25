a22203b4987d094191010a8898b45dc5a389d783
Anand Kothari <kotharan@oregonstate.edu>
2018-09-25 03:45:43 -0700

    Added extra code to learn types

--- C++_Code_Solutions/ContainsDuplicate.cpp
+++ C++_Code_Solutions/ContainsDuplicate.cpp
@@ -29,6 +29,48 @@ public:
 };
 
 
+//unordered_set<int> isFound;
+////If there are more duplicates than one and you wan to print all duplicates
+//
+//class Solution {
+//public:
+//    bool containsDuplicate(vector<int>& nums) {
+//    unordered_set<int> m;
+//    for(int i=0;i<nums.size();i++)
+//    {
+//        if(m.find(nums[i])!= m.end()) // If it finds the duplicate
+//        {
+//            cout<<"It has a duplicate which is:: " <<nums[i]<<endl;
+//            helper(nums[i]);
+//        }
+//        else
+//        {
+//            m.insert(nums[i]);
+//        }
+//
+//    }
+//    if(!helperCalled)
+//    {
+//    cout<<"No Duplicate found";
+//    return false;
+//    }
+//    }
+//
+//
+//bool helper(int num)
+//{
+//    if(isFound.find(num) == isFound.end()) // Duplicate is not found
+//    {
+//        isFound.insert(num);
+//        helperCalled = true;
+//    }
+//    else
+//    {
+//    return true;
+//    }
+//}
+//};
+
 main()
 {
     Solution s;
