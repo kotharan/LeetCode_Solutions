900eae18266901952ace3bb378921174b4acd98f
Anand Kothari <kotharan@oregonstate.edu>
2018-09-27 22:25:06 -0700

    Deletion function added

--- C++_Code_Solutions/NodeDelete.cpp
+++ C++_Code_Solutions/NodeDelete.cpp
@@ -21,10 +21,38 @@ void push(struct Node** head_ref, int new_data)  // O(1) :: Time complexity
 
 }
 
-void delNode( struct Node** head , int data)
+// This function finds for the key which needs to
+// be deleted and return nothing if it doesn't have the key
+void delNode( struct Node** head_ref , int key)
 {
+    struct Node* temp = *head_ref , *prev;
+    if(temp != NULL && temp->data == key)
+    {
+        *head_ref = temp->next;
+        free(temp);
+        return;
+    }
+
+    // We need traverse the entire list for the key
+    // and also keep track of the previous node
+    // so that we could change the "prev->next" after deletion
+
+    while(temp !=NULL && temp->data != key)
+    {
+        prev = temp;
+        temp = temp->next;
+    }
+
+    // If key not found
+    if(temp == NULL) return;
+
+    // Remove the deletion key and point to its next node
+    prev->next = temp->next;
+    free(temp);  // Free memory after deletion
+
 
 }
+
 
 
 int main()
