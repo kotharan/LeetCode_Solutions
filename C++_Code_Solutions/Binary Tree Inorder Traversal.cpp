e90fa981521dfd1e4a4705ea2e246f7bb54cb9cc
Anand Kothari <kotharan@oregonstate.edu>
2018-10-05 21:54:59 -0700

    Adding Tree Inorder Traversal.cpp

---
+++ C++_Code_Solutions/Binary Tree Inorder Traversal.cpp
@@ -0,0 +1,36 @@
+//Done By >> Anand S Kothari
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode(int x) : val(x), left(NULL), right(NULL) {}
+ * };
+ */
+
+
+class Solution {
+public:
+    vector<int> inorderTraversal(TreeNode* root) {
+        vector<TreeNode*> stack;
+        vector<int> res;
+        
+        while(root!=NULL || stack.size()>0)
+        {
+            if(root!=NULL)
+            {
+                stack.push_back(root);
+                root = root->left; 
+            }else
+            {
+                root = stack.back(); // Go back to the stack value 
+                stack.pop_back();       
+                res.push_back(root->val);
+                root = root->right;
+                
+            }
+        }
+      return res;  
+    }
+};
