4e210a48eda5bf12c5874b21534b6d534a87868c
Anand Kothari <kotharan@oregonstate.edu>
2019-01-13 18:10:49 -0800

    LinkedList

--- Python_Code_Solutions/Create_LL_Nodes.py
+++ Python_Code_Solutions/Create_LL_Nodes.py
@@ -1,13 +1,13 @@
-
 '''
 Done By -> Anand Kothari 
 The nodes are created by implementing a class which will hold the pointers along with the data element.
 '''
-class dayname:
-    def __init__(self,dataval = None):
-        self.dataval = dataval          # To Store value of  a nodes
-        self.nextval = None             # Pointer is initialized to null
 
+
+class dayname:
+    def __init__(self, dataval=None):
+        self.dataval = dataval  # To Store value of  a nodes
+        self.nextval = None  # Pointer is initialized to null
 
 
 d1 = dayname('Mon')
@@ -21,17 +21,15 @@ d1.nextval = d4
 d4.nextval = d2
 d2.nextval = d3
 
-# We have created the nodes now and also created pointers 
+# We have created the nodes now and also created pointers
 
-# Lets TRAVERSE 
+# Lets TRAVERSE
 # Consider "thisvalue" as the head pointer [This is where the linked list start], it will be used to traverse the node elements
-thisvalue  = d1  
+thisvalue = d1
 
 while thisvalue:
     print(thisvalue.dataval)
     thisvalue = thisvalue.nextval
-
-
 '''
 Output:
 -------
